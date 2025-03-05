8658939d99e0328b1645d6a29d3f08e23963ebf6
Ali Unwala <aliunwaladev@gmail.com>
2025-03-04 18:08:53 -0600

    Removing  partial prerendering

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experemental: {
-    ppr: "incremental",
-  },
 };
 
 export default nextConfig;
